22787d90c31e52a1bf86a878988fd4bf99c486d8
kevin <kevinelsen@hotmail.fr>
2018-07-31 11:39:44 +0200

    exo6

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 6</title>
+  </head>
+  <body>
+    <p>
+    <?php
+       //Boucle for, initialisé à 20. supérieur ou égal à 0 avec une décrémentation de 1
+      for ($floor = 20; $floor >= 0; $floor--) {
+        echo ' C\'est presque bon.';
+      }
+    ?>
+    </p>
+  </body>
+</html>
